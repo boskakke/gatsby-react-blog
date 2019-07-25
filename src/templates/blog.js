bac0614b5dce9a23c31105979f9b2a1e74e89e6b
Bo Skakke <bo@skakke.dk>
2019-07-25 22:56:22 +0200

    typo

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { documentToReactComponents} from '@Contentful/rich-text-react-renderer'
+import { documentToReactComponents} from '@contentful/rich-text-react-renderer'
 import Layout from '../components/layout'
 
 import Head from '../components/head'
